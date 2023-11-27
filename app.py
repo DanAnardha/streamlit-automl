6c61fb66c74fd6b538b0ca52f4f43156740f1b73
DanAnardha <danuar.anardha007@gmail.com>
2023-11-28 03:45:42 +0700

    add classification model visualization

--- app.py
+++ app.py
@@ -38,33 +38,18 @@ def df_report(df):
             mime="text/html",
             key="download_button_key"
         )
-    # st.subheader('2. Dataset Profiling (Optional)')
-    # st.info('Data profiling is the process of examining, analyzing, and creating useful summaries of data.')
-    # if st.checkbox("Analyze Dataset (may take a while)"):
-    #     with st.spinner("Creating dataset profile report..."):
-    #         profile_report = df.profile_report()
-    #         profile_report.to_file('report.html')
-
-    # path_to_html = "report.html"
-    # st.success('Creating dataset profiling success!')
-    # with open(path_to_html, 'r') as f:
-    #     html_data = f.read()
-    #     st.components.v1.html(html_data, height=768, scrolling=True)
-
-    # st.download_button(
-    #     label="Download Report",
-    #     data=html_data.encode('utf-8'),
-    #     file_name="report.html",
-    #     mime="text/html",
-    #     key="download_button_key"
-    # )
 
 def reg_automl():
     st.subheader("3. Training ML Model")
     features = st.multiselect("Select Features", df.columns)
     target = st.selectbox("Select Target", df.columns)
-    new_df = create_new_dataframe(df, features, target)
-    new_df
+    try:
+        new_df = create_new_dataframe(df, features, target)
+        new_df
+    except Exception as e:
+        st.warning("Cannot display the dataframe, possibly because you selected the same feature and target.")
+
+   
     if st.button("Train Model"):
         st.subheader("Selected Features and Target:")
         st.write("Features:", features)
@@ -81,6 +66,65 @@ def reg_automl():
         st.dataframe(compare_df)
         print(best_model)
         save_model(best_model, 'regression_model')
+
+def class_automl():
+    st.title("Machine Learning OKE!")
+    features = st.multiselect("Select Features", df.columns)
+    target = st.selectbox("Select Target", df.columns)
+    new_df = create_new_dataframe(df, features, target)
+    new_df
+    if st.button("Train Model"):
+        st.subheader("Selected Features and Target:")
+        st.write("Features:", features)
+        st.write("Target:", target)
+        with st.spinner("Running Machine Learning Experiment..."):
+            setup(new_df, target=target, verbose=False)
+            setup_df = pull()
+        st.info("This is ML experiment settings")
+        st.dataframe(setup_df)
+        with st.spinner("Comparing Models..."):
+            best_model = compare_models()
+            compare_df = pull()
+
+        st.info("This is the ML models")
+        st.dataframe(compare_df)
+        print(best_model)
+        save_model(best_model, 'classification_model')
+        plot_class(best_model)
+
+def plot_class(best_model):
+
+    st.subheader("Model Visualization:")
+
+    st.write("AUC Scores:")
+    try:
+        plot_model(best_model, plot = 'auc', display_format='streamlit')
+    except Exception as e:
+        st.warning("Cannot display the plot, possibly because the model does not support it.")
+
+    st.write("Boundary:")
+    try:
+        plot_model(best_model, plot = 'boundary', display_format='streamlit')
+    except Exception as e:
+        st.warning("Cannot display the plot, possibly because the model does not support it.")
+
+    st.write("Confusion Matrix:")
+    try:
+        plot_model(best_model, plot = 'confusion_matrix', display_format='streamlit')
+    except Exception as e:
+        st.warning("Cannot display the plot, possibly because the model does not support it.")
+
+    st.write("Precision-Recall Curve:")
+    try:
+        plot_model(best_model, plot = 'pr', display_format='streamlit')
+    except Exception as e:
+        st.warning("Cannot display the plot, possibly because the model does not support it.")
+
+    st.write("Feature Importance:")
+    try:
+        plot_model(best_model, plot = 'feature', display_format='streamlit')
+    except Exception as e:
+        st.warning("Cannot display the plot, possibly because the model does not support it.")
 
 # Sidebar
 with st.sidebar:
@@ -128,7 +172,10 @@ if file is not None:
     st.dataframe(df)
     st.info(f"Dataset dimension: {df.shape}")
     df_report(df)
-    reg_automl()
+    if option == "Regression":
+        reg_automl()
+    elif option == "Classification":
+        class_automl()
 else:
     st.info('Awaiting for CSV file to be uploaded.')
     if st.checkbox('Press to use Example Dataset'):
@@ -142,69 +189,14 @@ else:
         st.dataframe(df)
         st.info(f"Dataset dimension: {df.shape}")
         df_report(df)
-        reg_automl()
+        if option == "Regression":
+            reg_automl()
+        elif option == "Classification":
+            class_automl()
 
     if choice == "Download":
         with open("regression_model.pkl", 'rb') as f:
             st.download_button("Download Model", f, "regression_model.pkl")
-
-if option == "Classification":  
-    if choice == "ML":
-        st.title("Machine Learning OKE!")
-        features = st.multiselect("Select Features", df.columns)
-        target = st.selectbox("Select Target", df.columns)
-        new_df = create_new_dataframe(df, features, target)
-        new_df
-        if st.button("Train Model"):
-            st.subheader("Selected Features and Target:")
-            st.write("Features:", features)
-            st.write("Target:", target)
-            with st.spinner("Running Machine Learning Experiment..."):
-                setup(new_df, target=target, verbose=False)
-                setup_df = pull()
-            st.info("This is ML experiment settings")
-            st.dataframe(setup_df)
-            with st.spinner("Comparing Models..."):
-                best_model = compare_models()
-                compare_df = pull()
-
-            st.info("This is the ML models")
-            st.dataframe(compare_df)
-            print(best_model)
-
-            st.subheader("Model Visualization:")
-
-            st.write("AUC Scores:")
-            try:
-                plot_model(best_model, plot = 'auc', display_format='streamlit')
-            except Exception as e:
-                st.warning("Cannot display the plot, possibly because the model does not support it!")
-
-            st.write("Boundary:")
-            try:
-                plot_model(best_model, plot = 'boundary', display_format='streamlit')
-            except Exception as e:
-                st.warning("Cannot display the plot, possibly because the model does not support it!")
-
-            st.write("Confusion Matrix:")
-            try:
-                plot_model(best_model, plot = 'confusion_matrix', display_format='streamlit')
-            except Exception as e:
-                st.warning("Cannot display the plot, possibly because the model does not support it!")
-
-            st.write("Precision-Recall Curve:")
-            try:
-                plot_model(best_model, plot = 'pr', display_format='streamlit')
-            except Exception as e:
-                st.warning("Cannot display the plot, possibly because the model does not support it!")
-
-            st.write("Feature Importance:")
-            try:
-                plot_model(best_model, plot = 'feature', display_format='streamlit')
-            except Exception as e:
-                st.warning("Cannot display the plot, possibly because the model does not support it!")
-
-            save_model(best_model, 'classification_model')
 
     if choice == "Download":
         with open("classification_model.pkl", 'rb') as f:
