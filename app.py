d6df53555728c1d05f8f2b8757db77bde474cf7c
DanAnardha <danuar.anardha007@gmail.com>
2023-11-28 23:01:09 +0700

    fix code loop after click download

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def create_new_dataframe(df, features, target):
 
 def df_report(df):
     st.subheader('2. Dataset Profiling (Optional)')
-    if st.checkbox("Analyze Dataset"):
+    if st.checkbox("Analyze Dataset (may take a while)"):
         if "profile_report" not in st.session_state:
             with st.spinner("Creating dataset profile report..."):
                 profile_report = df.profile_report()
@@ -40,7 +40,8 @@ def df_report(df):
         )
 
 def reg_automl():
-    st.subheader("3. Training ML Model")
+    st.subheader("3. Train Regression Model")
+    st.info('Choose features and targets carefully and systematically.')
     features = st.multiselect("Select Features", df.columns)
     target = st.selectbox("Select Target", df.columns)
     try:
@@ -49,7 +50,7 @@ def reg_automl():
     except Exception as e:
         st.warning("Cannot display the dataframe, possibly because you selected the same feature and target.")
 
-   
+    st.info('You can set the training data split ratio and random seed number on the sidebar.')
     if st.button("Train Model"):
         st.subheader("Selected Features and Target:")
         st.write("Features:", features)
@@ -66,35 +67,51 @@ def reg_automl():
         st.dataframe(compare_df)
         print(best_model)
         save_model(best_model, 'regression_model')
+        st.subheader('5. Download Model')
+        st.info('The model you downloaded is the model with the highest accuracy.')
+        with open("regression_model.pkl", 'rb') as f:
+            st.download_button("Download Model", f, "regression_model.pkl")
 
-def class_automl():
-    st.title("Machine Learning OKE!")
+def class_automl(split_size, seed_number):
+    st.subheader("3. Train Classification Model")
+    st.info('Choose features and targets carefully and systematically.')
     features = st.multiselect("Select Features", df.columns)
     target = st.selectbox("Select Target", df.columns)
-    new_df = create_new_dataframe(df, features, target)
-    new_df
+    try:
+        new_df = create_new_dataframe(df, features, target)
+        new_df
+    except Exception as e:
+        st.warning("Cannot display the dataframe, possibly because you selected the same feature and target.")
+
+    st.info('You can set the training data split ratio and random seed number on the sidebar.')
     if st.button("Train Model"):
         st.subheader("Selected Features and Target:")
         st.write("Features:", features)
         st.write("Target:", target)
-        with st.spinner("Running Machine Learning Experiment..."):
-            setup(new_df, target=target, verbose=False)
-            setup_df = pull()
-        st.info("This is ML experiment settings")
-        st.dataframe(setup_df)
-        with st.spinner("Comparing Models..."):
-            best_model = compare_models()
-            compare_df = pull()
-
-        st.info("This is the ML models")
-        st.dataframe(compare_df)
-        print(best_model)
-        save_model(best_model, 'classification_model')
-        plot_class(best_model)
+        if "model_saved" not in st.session_state:
+            with st.spinner("Running Machine Learning Experiment..."):
+                setup(new_df, target=target, train_size=split_size, session_id=seed_number, verbose=False)
+                setup_df = pull()
+            st.info("This is ML experiment settings")
+            st.dataframe(setup_df)
+            with st.spinner("Comparing Models..."):
+                best_model = compare_models()
+                compare_df = pull()
+            st.info("This is the ML models")
+            st.dataframe(compare_df)
+            print(best_model)
+            save_model(best_model, 'classification_model')
+            st.session_state.model_saved = True
+            plot_class(best_model)
+        st.subheader('5. Download Model')
+        st.info('The model you downloaded is the model with the highest accuracy.')
+        if st.session_state.model_saved:
+            with open("classification_model.pkl", 'rb') as f:
+                st.download_button("Download Model", f, "classification_model.pkl")
 
 def plot_class(best_model):
 
-    st.subheader("Model Visualization:")
+    st.subheader("4. Model Visualization")
 
     st.write("AUC Scores:")
     try:
@@ -133,9 +150,8 @@ with st.sidebar:
     st.header('1. Upload your CSV data')
     file = st.sidebar.file_uploader("Upload Dataset Here:", type=["csv", "xlsx", "json", 'data'])
     st.header('2. Set Parameters')
-    split_size = st.sidebar.slider('Data split ratio (% for Training Set)', 10, 90, 80, 5)
+    split_size = st.sidebar.slider('Data split ratio (% for Training Set)', 10, 90, 80, 5) / 100
     seed_number = st.sidebar.slider('Set the random seed number', 1, 100, 42, 1)
-    # st.header("Pengaturan")
     choice = st.radio("Navigation", ["Upload", "Profiling", "ML", "Download"])
 
 try:
@@ -175,7 +191,7 @@ if file is not None:
     if option == "Regression":
         reg_automl()
     elif option == "Classification":
-        class_automl()
+        class_automl(split_size, seed_number)
 else:
     st.info('Awaiting for CSV file to be uploaded.')
     if st.checkbox('Press to use Example Dataset'):
@@ -192,12 +208,4 @@ else:
         if option == "Regression":
             reg_automl()
         elif option == "Classification":
-            class_automl()
-
-    if choice == "Download":
-        with open("regression_model.pkl", 'rb') as f:
-            st.download_button("Download Model", f, "regression_model.pkl")
-
-    if choice == "Download":
-        with open("classification_model.pkl", 'rb') as f:
-            st.download_button("Download Model", f, "classification_model.pkl")
+            class_automl(split_size, seed_number)
